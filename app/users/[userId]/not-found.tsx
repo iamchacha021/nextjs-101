accd1438357642244de9b5588b20724fe7233efa
iamchacha021 <iamchacha021@gmail.com>
2023-09-13 08:30:50 +0300

    Delete users

--- app/users/[userId]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NotFound() {
-  return (
-    <div>
-        <h1>This User Doesn't Exist</h1>
-    </div>
-  )
-}
--- lib/getUserPosts.tsx
+++
@@ -1,7 +0,0 @@
-
-export default async function getUserPosts(userId: string) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { next: { revalidate: 60 }})
-    if(!res.ok) undefined
-
-    return res.json()
-}
